17d620c1fc622d5b91ce979549f4e8a15e60ddff
MichalPysik <n3r0x541@gmail.com>
2024-03-30 15:29:27 +0100

    introduced data augmentation technique inducing hallucinations

--- src/baseline_solution.py
+++ src/baseline_solution.py
@@ -1,9 +1,10 @@
 import whisper
-import librosa
+import soundfile as sf
 import os
 import torch as t
 import string
 from hallucination_metrics import check_hallucinations
+from data_augmentation import augment_short_audio
 
 # Load the baseline model
 model = whisper.load_model("large-v3")
@@ -15,7 +16,7 @@ number_tokens = [
     if all(c in "0123456789" for c in tokenizer.decode([i]).removeprefix(" "))
 ]
 
-directory = "data/test-other/LibriSpeech/test-other/367/130732"
+directory = "data/test-other/LibriSpeech/test-other/8280/266249"
 X_test = []
 y_true = []
 
@@ -23,22 +24,32 @@ y_true = []
 entries = sorted(os.scandir(directory), key=lambda x: x.name)
 for entry in entries:
     if entry.path.endswith(".flac") and entry.is_file():
-        audio, _ = librosa.load(entry.path, sr=None)
-        X_test.append(audio)
+        audio, sr = sf.read(entry.path)
+        augmented_audio = augment_short_audio(audio, sr)
+        X_test.append(augmented_audio)
+        # save the augmented audio to tmp folder
+        sf.write(f"tmp/{entry.name[:-5]}_augmented.flac", augmented_audio, sr)
 
-# Load transcripts sorted lexicographically (lowercase, no punctuation)
-with open(directory + "/367-130732.trans.txt") as tf:
+# Load transcripts sorted lexicographically (lowercase, no punctuation except ')
+with open(directory + "/8280-266249.trans.txt") as tf:
     for line in tf:
         y_true.append(line.split(" ", 1)[1][:-1].lower().strip())
 
-# Predict the transcriptions (lowercase, no punctuation)
+# Predict the transcriptions (lowercase, no punctuation except ')
 y_pred = []
 for audio in X_test:
     trans_punct = model.transcribe(audio, suppress_tokens=number_tokens)["text"].lower().strip()
-    trans = trans_punct.translate(str.maketrans('', '', string.punctuation))
+    trans = trans_punct.translate(str.maketrans('', '', string.punctuation.replace("\'", "")))
     y_pred.append(trans)
 
 _ = check_hallucinations(y_true, y_pred, 0.3, 0.2, 200, verbose=True)
+
+for i in range(len(y_true)):
+    if y_true[i] == y_pred[i]:
+        continue
+    print("True:", y_true[i])
+    print("Pred:", y_pred[i])
+    print("\n")
 
 
 
---
+++ src/data_augmentation.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+# Augments a (short) audioclip by adding a pause, and white noise
+def augment_short_audio(audio, sample_rate, add_noise=True, add_pause=True):
+    if add_pause:
+        silence = np.zeros(np.random.randint(sample_rate * 3, sample_rate * 30))
+        random_index = np.random.randint(0, len(audio))
+        audio1, audio2 = np.split(audio, [random_index])
+        _augmented_audio = np.concatenate([audio1, silence, audio2])
+    else:
+        _augmented_audio = audio
+
+    if add_noise:
+        noise = np.random.normal(scale=0.0025, size=len(_augmented_audio))
+        augmented_audio = (_augmented_audio + noise)
+    else:
+        augmented_audio = _augmented_audio
+    
+    return augmented_audio.astype(np.float32)
