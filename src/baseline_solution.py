0acf2df169a46c347f0ce6fdf673f87e49afb954
MichalPysik <n3r0x541@gmail.com>
2024-04-13 18:29:11 +0200

    quantitative test results

--- src/baseline_solution.py
+++ src/baseline_solution.py
@@ -6,6 +6,36 @@ import string
 from hallucination_detection import detect_hallucinations_article, detect_hallucinations_simple
 from data_augmentation import augment_short_audio
 
+
+def load_and_augment_dataset(base_directory):
+    X_test = []
+    y_true = []
+
+    # Iterate subdirectories of base_directory
+    for directory in os.scandir(base_directory):
+        if not directory.is_dir():
+            continue
+        # Iterate subdirectories of the subdirectory
+        for subdirectory in os.scandir(directory.path):
+            if not subdirectory.is_dir():
+                continue
+            print(subdirectory.path)
+            # Iterate audio files sorted lexicographically and augment them
+            entries = sorted(os.scandir(subdirectory.path), key=lambda x: x.name)
+            for entry in entries:
+                if entry.path.endswith(".flac") and entry.is_file():
+                    audio, sr = sf.read(entry.path)
+                    augmented_audio = augment_short_audio(audio, sr)
+                    X_test.append(augmented_audio)
+                elif entry.path.endswith(".trans.txt") and entry.is_file():
+                    with open(entry.path) as tf:
+                        for line in tf:
+                            y_true.append(line.split(" ", 1)[1][:-1].lower().strip())
+
+
+    return X_test, y_true
+
+
 # Load the baseline model
 model = whisper.load_model("large-v3")
 
@@ -16,24 +46,9 @@ number_tokens = [
     if all(c in "0123456789" for c in tokenizer.decode([i]).removeprefix(" "))
 ]
 
-directory = "data/test-other/LibriSpeech/test-other/4350/9170"
-X_test = []
-y_true = []
-
-# Load audio files sorted lexicographically and augment them
-entries = sorted(os.scandir(directory), key=lambda x: x.name)
-for entry in entries:
-    if entry.path.endswith(".flac") and entry.is_file():
-        audio, sr = sf.read(entry.path)
-        augmented_audio = augment_short_audio(audio, sr)
-        X_test.append(augmented_audio)
-        # save the augmented audio to tmp folder
-        sf.write(f"tmp/{entry.name[:-5]}_augmented.flac", augmented_audio, sr)
-
-# Load transcripts sorted lexicographically (lowercase, no punctuation except ')
-with open(directory + "/4350-9170.trans.txt") as tf:
-    for line in tf:
-        y_true.append(line.split(" ", 1)[1][:-1].lower().strip())
+# Load and augment the dataset
+base_directory = "data/test-other/LibriSpeech/test-other"
+X_test, y_true = load_and_augment_dataset(base_directory)
 
 # Predict the transcriptions (lowercase, no punctuation except ')
 y_pred = []
@@ -42,17 +57,13 @@ for audio in X_test:
     trans = trans_punct.translate(str.maketrans('', '', string.punctuation.replace("\'", "")))
     y_pred.append(trans)
 
-# Print the detected hallucinations
-_ = detect_hallucinations_simple(y_true, y_pred, verbose=True)
+# Detect hallucinations and save the results
+results = detect_hallucinations_simple(y_true, y_pred, verbose=True)
 
-exit()
-# Manual (human) check of hallucinations
-for i in range(len(y_true)):
-    if y_true[i] == y_pred[i]:
-        continue
-    print("True:", y_true[i])
-    print("Pred:", y_pred[i])
-    print("\n")
+# Save the results to results.txt
+with open("results.txt", "w") as f:
+    for key, value in results.items():
+        f.write(f"{key}: {value}\n")
 
 
 
